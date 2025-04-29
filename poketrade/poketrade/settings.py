cd32add1d569b66cd6668d12b586231a2e5dc92b
rithikdhivakar <rithikdhiva@gmail.com>
2025-04-29 17:18:45 -0400

    Fix password-reset email flow

--- poketrade/poketrade/settings.py
+++ poketrade/poketrade/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhaaraths20@gmail.com'        
-EMAIL_HOST_PASSWORD = 'geyw baxp rmso meuj'   # ✅ Replace with Gmail App Password
+EMAIL_HOST_USER = 'rithikdhiva@gmail.com'
+EMAIL_HOST_PASSWORD = 'bzbs nkrs inqm kcwn'   # ✅ Replace with Gmail App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 import os
